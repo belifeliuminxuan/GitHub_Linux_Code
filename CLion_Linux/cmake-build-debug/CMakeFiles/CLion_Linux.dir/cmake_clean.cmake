eb0e95e1d7430b52822a26168a51fa91c1194db3
刘敏轩 <1187060103@qq.com>
2020-02-13 04:33:00 +0800

    0212

---
+++ CLion_Linux/cmake-build-debug/CMakeFiles/CLion_Linux.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLion_Linux.exe"
+  "CLion_Linux.exe.manifest"
+  "CLion_Linux.pdb"
+  "CMakeFiles/CLion_Linux.dir/main.cpp.obj"
+  "libCLion_Linux.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_Linux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
