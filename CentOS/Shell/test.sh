db99aef21adda87414bf9374212c99151dae3005
liuminxuan <liuminxuan@localhost.localdomain>
2020-02-19 20:55:33 +0800

    0219

---
+++ CentOS/Shell/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#touch ./hello.c
+#ls -a
+#cp hello.c ../C
+
---
+++ CentOS/Shell/variable.sh
@@ -0,0 +1,9 @@
+#message="Hello World"
+#echo $message
+
+#read message 
+#echo "this is my $message"
+
+read frist 
+read second 
+echo "my first name is :$frist ,my second name is : $second "
