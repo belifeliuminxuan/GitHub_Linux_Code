5a878ae236ac5efb542f9474dd7d897ca8e1bea5
liuminxuan <1187060103@qq.com>
2020-02-21 19:47:09 +0800

    0220

--- CentOS/C/test_1.c
+++ CentOS/C/test_1.c
@@ -1,8 +1,17 @@
 /**********************************************************
  * Author   : liuminxuan
  * Email    : liuminxuan1024@163.com
- * Time     : 2020-02-21 15:52
+ * Time     : 2020-02-21 17:01
  * FileName : test_1.c
  * *******************************************************/
 
+#include <stdio.h>
+#include <ctype.h>
 
+int main()
+{
+  printf ("%c",tolower('A'));
+
+
+  return 0;
+}
