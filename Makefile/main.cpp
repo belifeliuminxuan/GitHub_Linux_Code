1a7ca30c1eaf36050bd3f0faf9e13d27a2d40102
liuminxuan <1187060103@qq.com>
2020-03-08 22:08:12 +0800

    0308

---
+++ Makefile/main.cpp
@@ -0,0 +1,13 @@
+/**********************************************************
+ * Author   : liuminxuan
+ * Email    : liuminxuan1024@163.com
+ * Time     : 2020-03-08 22:04
+ * FileName : main.cpp
+ * *******************************************************/
+#include "test.h"
+
+int main()
+{
+cout<<"123"<<endl;
+  return 0;
+}
---
+++ Makefile/test.h
@@ -0,0 +1,10 @@
+/**********************************************************
+ * Author   : liuminxuan
+ * Email    : liuminxuan1024@163.com
+ * Time     : 2020-03-08 22:03
+ * FileName : test.h
+ * ********************************************************/
+#pragma once
+#include <iostream>
+using namespace std;
+
